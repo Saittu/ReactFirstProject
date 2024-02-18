204576bef0d10d2d37f1a824761ef1cfbf3b09e7
Jean Santos <jeanants@icloud.com>
2024-02-18 04:42:36 -0300

    adjusted post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
 import { Post } from './components/Post/Post'
 import { Header } from './components/Header'
 import { SideBar } from './components/SideBar/SideBar'
+
  
 import './global.css'
 import styles from './App.module.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      AvatarUrl: 'https://i.pinimg.com/75x75_RS/4d/78/9a/4d789a20ce3303751cb656b47550908a.jpg',
+      name: 'Jane Floren',
+      role: "UI"
+    },
+    content: [
+                { type: 'paragraph', content: 'Fala galeraa 👋'},
+
+                { type: 'paragraph', content: 'Acabei de subir mas um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+
+                { type: 'link', content: 'jane.design/doctorcare' }, 
+    ],
+    publishedAt: new Date('2024-02-18 20:00:00')
+  },
+
+  {
+    id: 2,
+    author: {
+      AvatarUrl: 'https://i.pinimg.com/236x/94/d7/0f/94d70f63dfa0db8020b0333168a3b606.jpg',
+      name: 'Devon Lane',
+      role: "Dev front-end"
+    },
+    content: [
+                { type: 'paragraph', content: 'Fala pessoal 👋'},
+
+                { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻'},
+
+                { type: 'link', content: 'Acesse e deixe seu feedback 👉 devonlane.design' }, 
+    ],
+    publishedAt: new Date('2024-02-18 23:34:01')
+  },
+]
 
 export function App() {
   return (
@@ -13,8 +50,15 @@ export function App() {
       <div className={styles.wrapper}>
         <SideBar/>
         <main>
-            <Post />
-            <Post />
+            {posts.map(post =>{
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })}
         </main>
       </div>
     </div>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,38 +1,41 @@
+import { format, formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
+
 import style from "./Post.module.css"
 
 import { Comment } from "../Comments/Comment"
 import { Avatar } from "../Avatar/Avatar"
 
-export function Post(props) {
+export function Post({ author, publishedAt, content}) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBR})
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {locale: ptBR, addSuffix: true})
+
     return (
         <article className={style.Post}>
             <header>
                 <div className={style.author}>
-                    <Avatar src="https://i.pinimg.com/75x75_RS/4d/78/9a/4d789a20ce3303751cb656b47550908a.jpg" />
+                    <Avatar src={author.AvatarUrl} />
 
                     <div className={style.authorInfo}>
-                        <strong>Jane Floren</strong>
-                        <span>UI/UX</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="February 15th at 11 am" dateTime="2024-02-15 09:59:23">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={style.content}>
-
-                <p>Fala galeraa 👋</p>
-
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p><a href="#">jane.design/doctorcare</a></p>
-
-                <p>
-                    <a href="#">#novoprojeto </a>{' '}
-                    <a href="#">#nlw</a> {' '}
-                    <a href="#">#rocketseat</a>
-                </p>
-
+                {content.map(line => {
+                    if(line.type === 'paragraph'){
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link'){
+                        return <p><a href="#">{line.content}</a></p>
+                    }    
+                })}
             </div>
 
             <form className={style.commentForm}>
